7240428f034a17d23462aa90312d0695817cd114
evelynteng <103479677+evelynteng@users.noreply.github.com>
2025-01-29 17:52:18 +0800

    view counter

--- js/script.js
+++ js/script.js
@@ -4,5 +4,22 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7BL6UGwmsYDpOMb1orMxLGIxLsHi8iTU",
+  authDomain: "evelyns-website.firebaseapp.com",
+  projectId: "evelyns-website",
+  storageBucket: "evelyns-website.firebasestorage.app",
+  messagingSenderId: "436629502752",
+  appId: "1:436629502752:web:da3e40a4c5e86971693545",
+  measurementId: "G-1ZJBXNBW4Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("Firebase Initialized:", app); // Confirm if Firebase is initialized
 
 
